3fa8ff091ce4737bc18826e8ac050750b2f0a50e
Maximo Martinez Gonzalez <maximomrtnz@gmail.com>
2017-02-09 21:15:39 -0300

    Working on Authtentication functionality

---
+++ app/src/main/java/com/maximomrtnz/moneybox/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.maximomrtnz.moneybox.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.maximomrtnz.moneybox.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/maximomrtnz/moneybox/activities/MainActivity.java
+++ app/src/main/java/com/maximomrtnz/moneybox/activities/MainActivity.java
@@ -1,9 +1,15 @@
-package com.maximomrtnz.moneybox;
+package com.maximomrtnz.moneybox.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.maximomrtnz.moneybox.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
